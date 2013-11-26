bdc61c3dc99b0107a09d6cc8630165adf8278084
JRizzle88 <skatlib1@aim.com>
2013-11-26 16:14:38 -0600

    added host

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Verone::Application.initialize!
+
+# connect to host
+config.action_mailer.default_url_options = { :host => 'verone.heroku.com' }
