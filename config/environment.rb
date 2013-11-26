dae45a6b2d70667107654b56664fb3c075eda3d6
JRizzle88 <skatlib1@aim.com>
2013-11-26 16:17:01 -0600

    host change

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Verone::Application.initialize!
 
-# connect to host
-config.action_mailer.default_url_options = { :host => 'verone.heroku.com' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Verone::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => 'verone.heroku.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
