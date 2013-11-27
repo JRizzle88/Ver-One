928f7e931c6a40f37fa1a67f4b535a4c41423292
JRizzle88 <skatlib1@aim.com>
2013-11-26 18:00:30 -0600

    removed deprecation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,6 @@ Verone::Application.configure do
   # Code is not reloaded between requests.
   #config.cache_classes = true
 
-  config.whiny_nils = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
